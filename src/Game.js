fb1cd247f81d200bd2916c5467a070ad589e531c
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-20 23:07:05 -0700

    fix bugs on button display during rolling animation

--- src/Game.js
+++ src/Game.js
@@ -27,8 +27,8 @@ class Game extends Component {
         smallStraight: undefined,
         largeStraight: undefined,
         yahtzee: undefined,
-        chance: undefined
-      }
+        chance: undefined,
+      },
     };
     this.roll = this.roll.bind(this);
     this.doScore = this.doScore.bind(this);
@@ -40,57 +40,68 @@ class Game extends Component {
   }
 
   animateRoll() {
-    this.setState({rolling: true}, () => {
+    this.setState({ rolling: true }, () => {
       setTimeout(this.roll, 1000);
     });
   }
 
   roll(evt) {
     // roll dice whose indexes are in reroll
-    this.setState(st => ({
+    this.setState((st) => ({
       dice: st.dice.map((d, i) =>
         st.locked[i] ? d : Math.ceil(Math.random() * 6)
       ),
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
       rollsLeft: st.rollsLeft - 1,
-      rolling: false
+      rolling: false,
     }));
   }
 
   toggleLocked(idx) {
     // Freeze dice buttons when there are no rolls left
     if (this.state.rollsLeft > 0 && !this.state.rolling) {
-            // toggle whether idx is in locked or not
-        this.setState(st => ({
-          locked: [
-            // keep everything the same before the index
-            ...st.locked.slice(0, idx),
-            // at the index, flip the state
-            !st.locked[idx],
-            // and keep the rest of the array the same after the index
-            ...st.locked.slice(idx + 1)
-          ]
+      // toggle whether idx is in locked or not
+      this.setState((st) => ({
+        locked: [
+          // keep everything the same before the index
+          ...st.locked.slice(0, idx),
+          // at the index, flip the state
+          !st.locked[idx],
+          // and keep the rest of the array the same after the index
+          ...st.locked.slice(idx + 1),
+        ],
       }));
-    } 
+    }
   }
 
   doScore(rulename, ruleFn) {
     // evaluate this ruleFn with the dice and score this rulename
-    this.setState(st => ({
+    this.setState((st) => ({
       scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
       rollsLeft: NUM_ROLLS,
-      locked: Array(NUM_DICE).fill(false)
+      locked: Array(NUM_DICE).fill(false),
     }));
-    this.roll();
+    this.animateRoll();
+  }
+
+  displayRollInfo() {
+    const messages = [
+      "0 Rolls Left",
+      "1 Roll Left",
+      "2 Rolls Left",
+      "Starting Round",
+    ];
+
+    return messages[this.state.rollsLeft];
   }
 
   render() {
     return (
-      <div className='Game'>
-        <header className='Game-header'>
-          <h1 className='App-title'>Yahtzee!</h1>
+      <div className="Game">
+        <header className="Game-header">
+          <h1 className="App-title">Yahtzee!</h1>
 
-          <section className='Game-dice-section'>
+          <section className="Game-dice-section">
             <Dice
               dice={this.state.dice}
               locked={this.state.locked}
@@ -98,13 +109,17 @@ class Game extends Component {
               disabled={this.state.rollsLeft === 0}
               rolling={this.state.rolling}
             />
-            <div className='Game-button-wrapper'>
+            <div className="Game-button-wrapper">
               <button
-                className='Game-reroll'
-                disabled={this.state.locked.every(x => x) || this.state.rollsLeft === 0}
+                className="Game-reroll"
+                disabled={
+                  this.state.locked.every((x) => x) ||
+                  this.state.rollsLeft === 0 ||
+                  this.state.rolling
+                }
                 onClick={this.animateRoll}
               >
-                {this.state.rollsLeft} Rerolls Left
+                {this.displayRollInfo()}
               </button>
             </div>
           </section>
